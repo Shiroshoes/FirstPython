b722b719a6675594f5b21f977744b5ce7176fa42
Jeo-Criz Izzack E. Perdio <jeocriz@gmail.com>
2025-04-29 17:26:11 +0800

    Create pythonstudylecture4.py

---
+++ pythonstudylecture4.py
@@ -0,0 +1,6 @@
+# restudy of python programming
+#Jeo-Criz Izzack E. Perdio
+# Data Science
+
+#Madlib gaming = this is a word game where we can create a story
+# by filling the blanks with random wordings
