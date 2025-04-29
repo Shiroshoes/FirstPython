20feb46aab9eedc5779b23521fe36d7220e9fbe0
Jeo-Criz Izzack E. Perdio <jeocriz@gmail.com>
2025-04-29 16:29:00 +0800

    Create pythonstudylecture2.py

---
+++ pythonstudylecture2.py
@@ -0,0 +1,43 @@
+# restudy of python programming
+#Jeo-Criz Izzack E. Perdio
+# Data Science
+
+#Typecasting is the process of converting the variable from one type to othe
+#THese are = str(), int(), float(), bool()
+
+print("Typecasting")
+name = "JEO"
+print(type(name))
+age = 19
+print(type(age))
+GWA = 1.5
+print(type(GWA))
+student = True
+print(type(student))
+
+#Changing the Float to Integers
+print("TYPECASTING - int()")
+new = int(GWA)
+print(new)
+print("\n")
+
+#Changing the Integers to Float
+print("TYPECASTING - float()")
+fnew = float(age)
+print(fnew)
+print("\n")
+
+#Changing to String
+print("TYPECASTING - str()")
+snew = str(age), str(student)
+print(snew)
+print("\n")
+
+#Changing to Boolean
+print("TYPECASTING - bool()")
+bnew = bool(name)
+print(bnew)
+# IF you want false result
+Name = ""
+b2new = bool(Name)
+print(b2new)
