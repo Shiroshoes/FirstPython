5c5b04bbac6cf16dda832ae51e6535bcbf6191ac
Jeo-Criz Izzack E. Perdio <jeocriz@gmail.com>
2025-05-02 01:19:42 +0800

    Update pythonstudycalculator.py

--- pythonstudycalculator.py
+++ pythonstudycalculator.py
@@ -4,7 +4,7 @@
 
 
 #Calculator program python
-
+print("Calculator program python")
 operator = input("Enter an operator (+ - * /): ")
 num1 = float(input("Enter the 1st Number: "))
 num2 = float(input("Enter the 2nd Number: "))
@@ -35,3 +35,41 @@ else:
 #    print(result)
 #else:
 #    print("Error please input an operator")
+
+
+#Python weight converter
+print("\n")
+
+print("Python weight converter")
+
+weight = float(input("Enter your weight: "))
+unit = input("Kilogram or Pounds (K or L): ")
+
+if unit == 'K':
+    weight = weight * 2.20462
+    unit = "Lbs."
+    print(f"Your weight is: {round(weight, 1)} {unit}")
+elif unit == 'L':
+    weight = weight / 2.20462
+    unit = "Kgs."
+    print(f"Your weight is: {round(weight, 1)} {unit}")
+else:
+    print("Error no unit is inputed")
+
+
+#Temperature conversion program
+print("\n")
+
+print("Temperature conversion program")
+
+un = input("Is this the temperature in Celsius or Farenheit (C/F): ")
+temp = float(input("Enter the temperature: "))
+
+if un == 'C':
+    temp = round((temp * 9)/5 + 32, 1)
+    print(f"The temp in Farenheit is: {temp} F")
+elif un == 'F':
+    temp = round((temp - 32) * 5/9, 1)
+    print(f"The temp in Celcius is: {temp} C")
+else:
+    print("Error unit not found")
