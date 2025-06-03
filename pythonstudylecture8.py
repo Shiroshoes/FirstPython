8ecbf78287ecf6f95aa26ac7e35d244e8f8be38f
Jeo-Criz Izzack E. Perdio <jeocriz@gmail.com>
2025-06-04 00:24:39 +0800

    Create pythonstudylecture8.py

---
+++ pythonstudylecture8.py
@@ -0,0 +1,34 @@
+# restudy of python programming
+#Jeo-Criz Izzack E. Perdio
+# Data Science
+
+# conditional expression - is a one-line shortcut for the if else statement (ternary operator)
+# print or assign one of two values based on a condition X if condition else Y
+
+num = 7
+print("Positive" if num > 0 else "Negative")
+
+result = "Even" if num % 2 == 0 else "odd"
+print(result)
+
+
+a = 7
+b = 8
+maxnum = a if a > b else b
+minnum = a if a < b else b
+print(maxnum)
+print(minnum)
+
+
+age = 19
+stat = "Adult" if age >= 18 else "child"
+print(stat)
+
+
+temp = 35
+weather = "hot" if temp >= 28 else "Warm"
+print(weather)
+
+userrole = "admin"
+access = "Full access" if userrole == "admin" else "Limited Access"
+print(access)
